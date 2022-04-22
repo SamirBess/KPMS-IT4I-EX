b9be8b96fca8304e8c6bfd0391f9d3bc185d424a
SamirBess <samirveliky@gmail.com>
2022-04-22 23:09:43 +0200

    .

--- mnist/p1.sh
+++ mnist/p1.sh
@@ -12,4 +12,4 @@ echo "loaded R"
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
 # I want the number to divide 76 = 4*19
-time mpirun -np 4 Rscript p1.R
+time mpirun -np 4 Rscript p1.R --args 4 32
