7ceb6780e15d8f4d2ab7ff47ab62f024fe3e8722
SamirBess <samirveliky@gmail.com>
2022-04-22 17:25:54 +0200

    R -> r

--- mpi/rf_mpi.sh
+++ mpi/rf_mpi.sh
@@ -14,5 +14,5 @@ echo "loaded R"
 # Fix for warnings from libfabric/1.12 bug
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
-time mpirun --map-by ppr:64:node Rscript rf_mpi.R
+time mpirun --map-by ppr:64:node Rscript rf_mpi.r
 
