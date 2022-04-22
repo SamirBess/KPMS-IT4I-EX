b3f40db9f866983f1c6acb77a0b78e1d8f070bfe
SamirBess <samirveliky@gmail.com>
2022-04-22 21:31:31 +0200

    ch

--- p1.R
+++ p1.R
@@ -7,4 +7,12 @@ kolik <- comm.size() # kolik
 
 print(paste("Ahoj, jmenuji se Empi", jakej, "jsem jeden z ", kolik, "identickych sourozencu!"))
 
+x <- runif(1)
+
+print(paste("Me nahode cislo od 0 do 1 je :", x))
+
+soucet <- gather(x)
 finalize()
+
+print(paste("Jejich soucet je ",sum(soucet)))
+print(paste("Vektor nahodnych cisel :", soucet))
