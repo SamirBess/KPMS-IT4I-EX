699a263e7bb1b90a20f63c2010eea2bcab40349e
SamirBess <samirveliky@gmail.com>
2022-05-18 12:05:56 +0200

    2

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -9,8 +9,15 @@ pwd
 module load R
 echo "loaded R"
 
+## prevent warning when fork is used with MPI
+export OMPI_MCA_mpi_warn_on_fork=0
+export RDMAV_FORK_SAFE=1
+
+## Fix for warnings from libfabric/1.12 bug
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
+
 ## --args blas fork
-mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
+mpirun --map-by ppr:4:node Rscript mnist_rsvd.R --args 2 8
 
 #time Rscript mnist_rsvd.R 1
 #time Rscript mnist_rsvd.R 2
