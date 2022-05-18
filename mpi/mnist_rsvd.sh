9828632fd127ee6a7edc69d3d33f9e4774fbdaa7
SamirBess <99830147+SamirBess@users.noreply.github.com>
2022-05-18 11:58:59 +0200

    Update mnist_rsvd.sh

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#PBS -N mnist_svd_cv
-#PBS -l select=1:ncpus=128,walltime=00:50:00
-#PBS -q qexp
-#PBS -e mnist_svd_cv.e
-#PBS -o mnist_svd_cv.o
+#PBS -N mnist_rsvd
+#PBS -l select=1:mpiprocs=32
+#PBS -l walltime=00:15:00
 
 cd ~/KPMS-IT4I-EX/mpi
 pwd
@@ -11,13 +9,8 @@ pwd
 module load R
 echo "loaded R"
 
-module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
-export OMPI_MCA_mpi_warn_on_fork=0
-export RDMAV_FORK_SAFE=1
-
-
 ## --args blas fork
-#time Rscript mnist_rsvd.R --args 64 64 64
+mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
 
 #time Rscript mnist_rsvd.R 1
 #time Rscript mnist_rsvd.R 2
@@ -30,6 +23,3 @@ export RDMAV_FORK_SAFE=1
 #time Rscript mnist_rsvd.R 256
 
 
-
-time mpirun -np 1 Rscript mnist_rsvd.r 
-time mpirun -np 64 Rscript mnist_rsvd.r 
